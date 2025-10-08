46f4a798e3ea2efb8d26407173c3cb7a6f04030a
camwest5 <118239146+camwest5@users.noreply.github.com>
2025-10-08 12:29:18 +1000

    update user agent

--- sh-get.py
+++ sh-get.py
@@ -1,6 +1,7 @@
 import requests
 
-r = requests.get("https://api.library.uq.edu.au/v1/training_events?take=100&filterIds[]=104&ts=1745812216416")
+r = requests.get("https://api.library.uq.edu.au/v1/training_events?take=100&filterIds[]=104&ts=1745812216416",
+                 headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0"})
 
 body = r.content.decode()
 
